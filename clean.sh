375217d14487df31ed7735a5e6c952b33c14b9b0
KKKKKXY <myaploy@gmail.com>
2024-07-02 14:21:12 +0000

    Bash scripts for setting up ONOS/Atomix multi-domain architecture

--- clean.sh
+++ clean.sh
@@ -1,10 +1,13 @@
-docker ps -a
-docker stop $(sudo docker ps -a -q)
-docker rm $(sudo docker ps -a -q)
-docker network ls
-docker network rm onos-cluster-net
-rm tmp/cluster-*.json
-rm tmp/atomix-*.conf
-cd /tmp
-rm cluster-*.json
-rm atomix-*.conf
+# !/bin/bash
+
+# Clean VM enviroment for re-starting an ONOS/Atomix architecture
+sudo docker ps -a
+sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
+sudo docker network ls
+sudo docker network rm onos-cluster-net
+sudo rm tmp/cluster-*.json
+sudo rm tmp/atomix-*.conf
+# sudo cd /tmp
+# sudo rm cluster-*.json
+# sudo rm atomix-*.conf
